677fdbd65178d43515513fdc868389f97e7feb51
Narixius <nariman.movaffaghi@gmail.com>
2025-04-20 14:24:19 +0330

    fix(zro/tools): export toMerged function from es-toolkit/compat

--- packages/zro/src/utils/tools.ts
+++ packages/zro/src/utils/tools.ts
@@ -1,7 +1,1 @@
-import { set, merge, cloneDeep, upperFirst } from "es-toolkit/compat";
-
-export const toMerged = <O, S>(object: O, source: S) => {
-  return merge(cloneDeep(object), source);
-};
-
-export { set, merge, cloneDeep, upperFirst };
+export { set, merge, cloneDeep, upperFirst, toMerged } from "es-toolkit/compat";
