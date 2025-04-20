819ec17b2cc04378af07db599726e9978385d504
Narixius <nariman.movaffaghi@gmail.com>
2025-04-20 15:18:37 +0330

    feat(connectors): add libsql connectors for HTTP, Node, and Web

---
+++ plugins/db/connectors/libsql/node.ts
@@ -0,0 +1,3 @@
+export * from "db0/connectors/libsql/node";
+import connector from "db0/connectors/libsql/node";
+export default connector;
---
+++ plugins/db/connectors/libsql/web.ts
@@ -0,0 +1,3 @@
+export * from "db0/connectors/libsql/web";
+import connector from "db0/connectors/libsql/web";
+export default connector;
