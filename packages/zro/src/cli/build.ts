4ed20bdc7ded52e823d69f21c4a327bb06d8357f
Narixius <nariman.movaffaghi@gmail.com>
2025-04-28 15:55:47 +0330

    refactor(zro): decouple load env from build output

--- packages/zro/src/cli/build.ts
+++ packages/zro/src/cli/build.ts
@@ -1,25 +1,5 @@
 import { cloudflare } from "@cloudflare/vite-plugin";
-import dotenv from "dotenv";
-import fs from "fs";
 import { build as viteBuild } from "vite";
-
-function loadAllEnv() {
-  const env = dotenv.parse(fs.readFileSync(".env")); // or .env.production if you have one
-  return env;
-}
-
-function generateProcessEnvShim() {
-  const env = loadAllEnv();
-  const entries = Object.entries(env).map(([key, value]) => {
-    return `${JSON.stringify(key)}: ${JSON.stringify(value)}`;
-  });
-  return `
-globalThis.process = globalThis.process || {};
-globalThis.process.env = Object.assign(globalThis.process.env || {}, { ${entries.join(
-    ","
-  )} });
-`.trim();
-}
 
 export const build = async () => {
   // Build client
@@ -62,9 +42,6 @@ export const build = async () => {
       ssr: true,
       rollupOptions: {
         input: "./.zro/router.server.ts",
-        output: {
-          banner: generateProcessEnvShim(),
-        },
       },
     },
   });
